3c12fd6d8f78a636a9d81bfc51b797dfc86acf0c
Rising Odegua <williamsrising592@gmail.com>
2018-05-24 13:48:01 +0100

    Individual product type db query working

--- functions/index.js
+++ functions/index.js
@@ -27,14 +27,35 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 //home page
-app.get('/', routes.home);  
+app.get('/', routes.home); 
+
+//Single product type
+app.post('/singletypes', (req,res)=>{
+  var producttype = req.body.producttype;
+  console.log("received :" + producttype);
+  var myRef = firebaseApp.database().ref('Products').child('Single_product_type').child(producttype);
+
+  getDatafromFirebase(myRef, producttype).then((data)=>{
+
+      getArrayFromFirebaseData(data).then((array) =>{
+        console.log(array);
+        // res.render('singletype', {producttypes: array});
+
+      });
+      
+  }).catch((err)=>{
+      console.log(err);
+  });
+
+});
 
 //Single product designs
 app.post('/designs', (req,res)=>{
   var productdesign = req.body.productdesign;
   console.log("received :" + productdesign);
+  var myRef = firebaseApp.database().ref('Products').child('Designs').child(productdesign);
 
-  getDatafromFirebase(productdesign).then((data)=>{
+  getDatafromFirebase(myRef, productdesign).then((data)=>{
       console.log(data);
       getArrayFromFirebaseData(data).then((array) =>{
 
@@ -48,6 +69,61 @@ app.post('/designs', (req,res)=>{
 
 });
 
+
+
+
+// app.get('/seeder', (req,res)=>{
+
+//   var types = [{
+//             imagesrc: 'https://res.cloudinary.com/printivo/image/upload/q_auto:eco/v1/printivo_s3/files/category_thumbnails/1440199066_1sidecards.png',
+//             name : 'One-Sided Business Cards',
+//             description:'Ideal for conferences, promotional events, trade expos and displays in your own store, our high-quality roll-up banner will help you increase your brand\'s visibility. Comes with retractable stands and carrier bags.',
+//             price: '₦22,999.00',
+//             oldprice:'₦24,999.00',
+//             size: '33 x 81 ',
+//             finishing:'Printing and mounted on Rollup Banner stand',
+//             delivery:'3 - 5 working days for Lagos, 5 - 7 working days for other cities',
+//             material:'Full colour digital printing on PVC '
+            
+//         },
+//       {
+//         imagesrc: 'https://res.cloudinary.com/printivo/image/upload/q_auto:eco/v1/printivo_s3/files/category_thumbnails/1440201227_two-sided-business-cards-by-printivo.png',
+//         name : 'Two-sided Business Cards',
+//         description:'Ideal for conferences, promotional events, trade expos and displays in your own store, our high-quality roll-up banner will help you increase your brand\'s visibility. Comes with retractable stands and carrier bags.',
+//         price: '₦22,999.00',
+//         oldprice:'₦24,999.00',
+//         size: '33 x 81 ',
+//         finishing:'Printing and mounted on Rollup Banner stand',
+//         delivery:'3 - 5 working days for Lagos, 5 - 7 working days for other cities',
+//         material:'Full colour digital printing on PVC '
+
+        
+//       },{
+        
+//         imagesrc: 'https://res.cloudinary.com/printivo/image/upload/q_auto:eco/v1/printivo_s3/files/category_thumbnails/1440202729_Square-business-cards.png',
+//         name : 'Square Business Cards (1 sided)',
+//         description:'Ideal for conferences, promotional events, trade expos and displays in your own store, our high-quality roll-up banner will help you increase your brand\'s visibility. Comes with retractable stands and carrier bags.',
+//         price: '₦22,999.00',
+//         oldprice:'₦24,999.00',
+//         size: '33 x 81 ',
+//         finishing:'Printing and mounted on Rollup Banner stand',
+//         delivery:'3 - 5 working days for Lagos, 5 - 7 working days for other cities',
+//         material:'Full colour digital printing on PVC '
+//       }]
+
+
+//   var myRef = firebaseApp.database().ref('Products').child('Single_product_type').child('bizcards');
+//         for(var i = 0; i < types.length;i++){
+//           myRef.push(types[i]).then(()=>{
+//             console.log('saved: ' + i);
+//           });
+//         }
+// });
+
+
+
+
+
 //Different types of banners
 app.get('/banners', routes.banners);
 
@@ -55,11 +131,10 @@ app.get('/banners', routes.banners);
 app.get('**', routes.errorpage)
 
 
-function getDatafromFirebase(productdesign){
+function getDatafromFirebase(db_reference,productdesign){
   return new Promise((resolve,reject)=>{
 
-      var myRef = firebaseApp.database().ref('Products').child('Designs').child(productdesign);
-      myRef.once("value").then((snapshot) => {
+      db_reference.once("value").then((snapshot) => {
           var data = snapshot.val();
           resolve(data); 
 
@@ -70,14 +145,14 @@ function getDatafromFirebase(productdesign){
   })
 }
 
-function getArrayFromFirebaseData(productdesigns){
+function getArrayFromFirebaseData(object_data){
   return new Promise((resolve,reject)=>{
-    var keys = Object.keys(productdesigns);
+    var keys = Object.keys(object_data);
     var array_of_productdesigns = []
     
     for(var i = 0; i < keys.length; i++){
       var key = keys[i];
-      array_of_productdesigns.push(productdesigns[key]);
+      array_of_productdesigns.push(object_data[key]);
     }
     resolve(array_of_productdesigns);
 
